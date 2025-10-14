4714467accaf614ca031fdd8ef3496d8e39cd506
Matvei Newod <martnew07@mail.ru>
2025-10-14 22:25:00 +0300

    delete everythink

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-class Config:
-
-    SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
-    REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-    JWT_SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
