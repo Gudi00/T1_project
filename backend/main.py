8c86d2c2e7de964024f45e25fdf22177bf972737
Egor <explayeroha@gmail.com>
2025-10-16 02:33:07 +0300

    mark down

--- backend/main.py
+++ backend/main.py
@@ -33,12 +33,12 @@ class Query(BaseModel):
 vector_db = VectorDataBase(api_token=API_TOKEN)
 try:
     vector_db.load_from_file("vector_db.json")
-    print("✅ Загружена существующая векторная база.")
+    print("Загружена существующая векторная база.")
 except FileNotFoundError:
-    print("⚙️ Файл базы не найден, создаём новую...")
+    print("Файл базы не найден, создаём новую...")
     vector_db.create_from_csv("kb.csv")
     vector_db.save_to_file("vector_db.json")
-    print("✅ Векторная база создана и сохранена.")
+    print("Векторная база создана и сохранена.")
 
 # Объекты поиска и истории
 searcher = VectorSearcher(vector_db, API_TOKEN)
@@ -47,7 +47,7 @@ chat = ChatHistory()
 
 @app.get("/")
 def root():
-    return {"message": "Smart Support backend is running ✅"}
+    return {"message": "Smart Support backend is running"}
 
 
 @app.post("/ask")
